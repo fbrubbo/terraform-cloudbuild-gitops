405608bbcf5737e0442e7e1c8f1c49cab8bd4f54
Fernando Rubbo <fernandorubbo@google.com>
2019-05-08 17:07:33 -0300

    initial commit

--- modules/project/main.tf
+++ modules/project/main.tf
@@ -20,3 +20,17 @@ resource "google_project_iam_policy" "project_iam" {
   project = "${google_project.project.project_id}"
   policy_data = "${data.google_iam_policy.editors.policy_data}"
 }
+
+resource "google_project_services" "services" {
+  project = "${var.project}"
+  services = [
+    "cloudbuild.googleapis.com", 
+    "containerregistry.googleapis.com",
+    "pubsub.googleapis.com",
+    "logging.googleapis.com",
+    "storage-api.googleapis.com",
+    "serviceusage.googleapis.com",
+    "sourcerepo.googleapis.com",
+    "compute.googleapis.com"
+  ]
+}
